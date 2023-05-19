859509905ffd9c6376dcfd564b75132d11314e59
tboros1991 <tboros1991@gmail.com>
2023-05-19 23:04:53 +0200

    change app title, implement app logo

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 from stock_api import StockApi
+from PIL import ImageTk, Image  
 import customtkinter
 
 stockApi = StockApi()
@@ -16,10 +17,13 @@ class AssistantInterface:
     root = customtkinter.CTk()
     root.geometry("500x350")
     
+    front_image = Image.open("./Pictures/logo.png")
+    test = ImageTk.PhotoImage(front_image)
+    
     frame = customtkinter.CTkFrame(master=root)
     frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-    label =customtkinter.CTkLabel(master=frame, text="Diagramm Drawer", font=("Roboto", 24)) # Custom text
+    label =customtkinter.CTkLabel(master=frame, text="Stock Exchange Assistant", font=("Roboto", 24)) # Custom text
     label.pack(pady=12, padx=10)
 
     entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Company Symbol") # Input field
@@ -27,6 +31,9 @@ class AssistantInterface:
 
     button = customtkinter.CTkButton(master=frame, text="Draw diagramm", command=APIcall) # Button
     button.pack(pady=12, padx=10)
+    
+    label2 = customtkinter.CTkLabel(master=frame, image=test, text="")
+    label2.pack(side = "bottom", fill = "both", expand = "yes")
 
     root.mainloop()
 
